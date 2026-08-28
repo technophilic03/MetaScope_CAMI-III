0cd1aa71302d312c8d40b25b60e6cae01c6953a9
technophilic03 <leng@bu.edu>
2026-08-28 09:26:59 -0400

    add code for running MetaScope and building accession tax

--- db/build_accession_tax.R
+++ db/build_accession_tax.R
@@ -5,7 +5,5 @@ out <- "/home/yl2800/wejlab/work/Yaoan/CAMI-III/db/accessionTaxa.sql"
 
 read.names.sql(file.path(db, "names.dmp"), out)
 read.nodes.sql(file.path(db, "nodes.dmp"), out)
-read.accession2taxid(file.path(db, "nucl_gb.accession2taxid.gz"), out, vocal = TRUE)
-read.accession2taxid(file.path(db, "nucl_wgs.accession2taxid.gz"), out, vocal = TRUE)
-
+read.accession2taxid(file.path(db, c("nucl_gb.accession2taxid.gz", "nucl_wgs.accession2taxid.gz")), out, vocal=TRUE, overwrite=TRUE)
 message("Done building accession taxa!")
